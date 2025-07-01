a9518459185abe5a128460c33d3c666e863ee696
NateXS <thecoolgamer0003@gmail.com>
2025-06-30 20:05:15 -0500

    added frametime measurements. turns out the bottleneck is actually creating and deleting images 😭

--- source/main.cpp
+++ source/main.cpp
@@ -40,6 +40,9 @@ int main(int argc, char **argv)
 	// this is for the FPS
 	std::chrono::_V2::system_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 	std::chrono::_V2::system_clock::time_point endTime = std::chrono::high_resolution_clock::now();
+	//this is for frametime check
+	std::chrono::_V2::system_clock::time_point frameStartTime = std::chrono::high_resolution_clock::now();
+	std::chrono::_V2::system_clock::time_point frameEndTime = std::chrono::high_resolution_clock::now();
 
 	if(!openScratchProject()){
 		exitApp();
@@ -57,9 +60,13 @@ int main(int argc, char **argv)
 		endTime = std::chrono::high_resolution_clock::now();
 		if(endTime - startTime >= std::chrono::milliseconds(1000 / FPS)){
 			startTime = std::chrono::high_resolution_clock::now();
+			frameStartTime = std::chrono::high_resolution_clock::now();
 			getInput();
 			BlockExecutor::runRepeatBlocks();
 			renderSprites();
+			frameEndTime = std::chrono::high_resolution_clock::now();
+			auto frameDuration = frameEndTime - frameStartTime;
+			std::cout << "\x1b[8;1HFrame time: " << frameDuration.count() << " ns" << std::endl;
 		}
 
 		gspWaitForVBlank();
