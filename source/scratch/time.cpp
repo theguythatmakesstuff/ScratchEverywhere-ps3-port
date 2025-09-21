ed119df3dd8a13ac9a9c64413e375e10639fd0a1
Turbotoad12 <jones_zachary@outlook.com>
2025-09-20 20:18:57 -0500

    chore: clean up `time.cpp` (#340)

--- source/scratch/time.cpp
+++ source/scratch/time.cpp
@@ -1,40 +1,7 @@
-// code mostly taken from devkitpro's time example, with edits needed for Scratch.
 #include <cstddef>
 #include <ctime>
 #include <time.hpp>
 
-/* const std::string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
-const std::string weekDays[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-const u16 daysAtStartOfMonthLUT[12] =
-    {
-        0 % 7,   // january    31
-        31 % 7,  // february   28+1(leap year)
-        59 % 7,  // march      31
-        90 % 7,  // april      30
-        120 % 7, // may        31
-        151 % 7, // june       30
-        181 % 7, // july       31
-        212 % 7, // august     31
-        243 % 7, // september  30
-        273 % 7, // october    31
-        304 % 7, // november   30
-        334 % 7  // december   31
-};
-
-static inline bool isLeapYear(int year) {
-    return (year % 4) == 0 && !((year % 100) == 0 && (year % 400) != 0);
-}
-
-static inline int getDayOfWeek(int day, int month, int year) {
-    // http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week
-    day += 2 * (3 - ((year / 100) % 4));
-    year %= 100;
-    day += year + (year / 4);
-    day += daysAtStartOfMonthLUT[month] - (isLeapYear(year) && (month <= 1));
-    return day % 7;
-} */
 
 int Time::getHours() {
     time_t unixTime = time(NULL);
